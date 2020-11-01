6fe57bd3df0a07858394f775225a3815870c8053
dmitriy-svintsov <72872027+dmitriy-svintsov@users.noreply.github.com>
2020-11-01 17:39:27 +0400

    Add files

---
+++ js/script.js
@@ -0,0 +1,1 @@
+var x=document.getElementById("contain");function myFunction(){document.getElementById("nav").classList.toggle("open"),x.classList.toggle("change")}x.addEventListener("click",myFunction);
