8597cd9cd18259a3fdb5f93aa5e355ec839535b0
dmitriy-svintsov <72872027+dmitriy-svintsov@users.noreply.github.com>
2020-12-05 10:43:34 +0400

    Ad slider

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+new Swiper('.image-slider', {
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,        
+   },
+});
+
+
 $(document).ready(function () {
 	$('.header__burger').click(function(event) {
 		$('.header__burger,.header__menu').toggleClass('active');
